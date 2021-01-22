ba6aee3adab505aa86ed99cd0bf10c9bdd1fa418
dreamcenter <1981669259@qq.com>
2021-01-22 21:44:14 +0800

    20210122-1-1_3

--- src/components/lihui.vue
+++ src/components/lihui.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <img id="df" :src="df1_now[0]" @mouseenter="test(0)" @mouseleave="test2(0)" @click="test3">
-    <img id="lh" :src="df1_now[1]" @mouseenter="test(1)" @mouseleave="test2(1)" @click="test3">
-    <audio :src="sound" ref="audio"></audio>
+    <img id="df" :src="df1_now[0]" @mouseenter="test(0)" @mouseleave="test2(0)" @click="test3(0)">
+    <img id="lh" :src="df1_now[1]" @mouseenter="test(1)" @mouseleave="test2(1)" @click="test3(1)">
+    <audio v-for="item in sound" :key="item" :src="item" ref="audio"></audio>
   </div>
 </template>
 
@@ -10,20 +10,42 @@
 export default {
   data () {
     return {
-      df1_now: ['./img/acgn/df1.png', './img/acgn/df2.png'],
+      df1_now: [],
       df1: ['./img/acgn/df1', './img/acgn/df2'],
-      sound: './sound/acgn/feed1.ogg'
+      // 音源二纯数恶搞(滑稽)
+      sound: ['./sound/acgn/feed1.ogg', './sound/acgn/hahaha.ogg']
     }
   },
+  mounted () {
+    this.df1_now = this.df1.map(function (item, index, arr) { return (item + '.png') })
+    this.preload()
+  },
   methods: {
+    preload () {
+      for (const item of this.df1) {
+        const img = new Image()
+        img.src = item + '.png'
+        img.onload = function () {
+          console.log(item + '.png>>>>>done!')
+        }
+        const imgAft = new Image()
+        imgAft.src = item + '_aft.png'
+        imgAft.onload = function () {
+          console.log(item + '_aft.png>>>>>done!')
+        }
+      }
+    },
     test (which) {
       this.$set(this.df1_now, which, this.df1[which] + '_aft.png')
     },
     test2 (which) {
       this.$set(this.df1_now, which, this.df1[which] + '.png')
     },
-    test3 () {
-      this.$refs.audio.play()
+    test3 (which) {
+      if (which === 1) {
+        this.$refs.audio[which].volume = 0.5
+      }
+      this.$refs.audio[which].play()
     }
   }
 }
